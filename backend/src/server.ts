68c5e94a4eb3fe77519bd58d3add51593e8ca96a
bilaltayyab121 <bilaltayyab121@gmail.com>
2025-08-28 20:39:21 +0500

    test case 14

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,34 +1,3 @@
-// // import app from './app';
-
-// // const PORT = process.env.port || 3000;
-
-// // const server = app.listen(PORT, () => {
-// //   console.log(`Server running on port ${PORT}`);
-// // });
-
-// // process.on('unhandledRejection', (err: Error) => {
-// //   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-// //   console.log(err.name, err.message);
-// //   server.close(() => {
-// //     process.exit(1);
-// //   });
-// // });
-
-// // process.on('SIGTERM', () => {
-// //   console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-// //   server.close(() => {
-// //     console.log('💥 Process terminated!');
-// //   });
-// // });
-
-// // export default app;
-
-// // backend/src/server.ts
-// import app from "./app";
-// import serverless from "serverless-http";
-
-// export const handler = serverless(app);
-
 import express from "express";
 
 const app = express();
